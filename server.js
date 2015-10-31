f4e10d0495b5d886b00f92e051651dbb018a0b9a
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-10-31 19:28:00 +0000

    Connection to CartoDB from NodeJS successful. Saves query results on JSON file, needs to send the data to EJS templates next.

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+// Configuration for connection to CartoDB's data
+// TODO: Put the CartoDB stuff in another file?
+var CartoDB = require('cartodb');
+var secret = require('./secret.js');
+var client = new CartoDB({user: secret.USER, api_key: secret.API_KEY});
+
+// Connect to CartoDB's namibmap account and send query
+client.on('connect', function() {
+    console.log("Connected to CartoDB! :-)");
+
+    //Retrieve list of all company names. Data is returned in JSON
+    var sql_companies = "SELECT company_name FROM companies";
+    client.query(sql_companies, function(err, data){
+    // JSON parsed data or error messages are returned
+    })
+});
+
+// Client is a stream object so you can pipe responses as new line delimited JSON
+// Saving response to file 
+// TODO: Can we pass this to EJS directly?
+var output = require('fs').createWriteStream(__dirname + '/companies.json');
+client.pipe(output);
+client.connect();
+
+//ExpressJS stuff
 var express = require('express'),
     app = express();
 
