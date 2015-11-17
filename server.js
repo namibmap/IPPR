7d2f6c1cda7e1bebdf28bec5736ff840ce7d3a4b
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2015-11-17 21:39:03 +0000

    calling geojson data and saving it in a json file; extracting companies and licenses data and serving it to ejs templates for front end

--- server.js
+++ server.js
@@ -1,45 +1,61 @@
 var express = require('express'),
     fs = require('fs'),
-    companies, modifyJSON, licenses_sorted;
+    Secret = require('./secret'),
+    request = require('request'),
+    jsonfile = require('jsonfile'),
+    data = require('./GeoData'),
+    data_obj = JSON.parse(data.body),
+    licenses_sorted = {},
+    companies_sorted = [];
 
-(function checkIfJSONExists(){
-    fs.exists('./companies.json', function(exists){
-        if (exists){
-            var updateJSON = require('./helpers/updateJSON').updateJSON;
-             // check if need to fetch JSON from CartoDB
-             updateJSON.checkFileTimeStamp();
-             createJSONVariables();
-        } else{
-            var CartoDbClient = require('./cartodb_client').CartoDbClient;
-            CartoDbClient.initialize();
-            createJSONVariables();
+
+(function sortOutData(){
+    for (i=0; i<data_obj.features.length; i++){
+            var key = data_obj.features[i].properties.license_number;
+            if (key in licenses_sorted) {
+                licenses_sorted[key].push(data_obj.features[i].properties.concession_number);
+            } else {
+                licenses_sorted[key] = [data_obj.features[i].properties.concession_number];
+            }
+            if ( companies_sorted.indexOf(data_obj.features[i].properties.company_name) <0 ){
+                companies_sorted.push(data_obj.features[i].properties.company_name);
+            }
         }
-    })
-})();
-
-function createJSONVariables(){
-    console.log('reading variables...');
-    setTimeout(function(){
-            companies = require('./companies.json'),
-            modifyJSON = require('./helpers/modifyJSON').modifyJSON,
-            licenses_sorted = modifyJSON.sortLicenses();
-    }, 2000);
-}
-
+    return licenses_sorted, companies_sorted;
+})()
 
 var app = express();
 
 app.set('view engine', 'ejs');
+
+request('https://namibmap.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT concessions.the_geom, concession_number, license_number, company_name FROM concessions, licenses, companies, license_holdings WHERE concessions.concession_license_id = licenses.license_id AND license_holdings.license_holding_company_id = companies.company_id AND license_holdings.license_holding_license_id = licenses.license_id&api_key='+Secret.API_KEY, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    jsonfile.writeFile(__dirname + '/GeoData.json', response, function(err){
+                    //console.log(err);
+                })
+  }
+})
+
 
 // serve static files from public folder
 app.use(express.static(__dirname +'/public'));
 
 app.get('/', function(req, res){
        res.render('pages/index', {
-            companies: companies,
-            licenses: licenses_sorted
+            data:data_obj,
+            licenses: licenses_sorted,
+            companies: companies_sorted
        });
 });
+
+request('https://namibmap.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT concessions.the_geom, concession_number, license_number, company_name FROM concessions, licenses, companies, license_holdings WHERE concessions.concession_license_id = licenses.license_id AND license_holdings.license_holding_company_id = companies.company_id AND license_holdings.license_holding_license_id = licenses.license_id LIMIT 1&api_key='+Secret.API_KEY, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    jsonfile.writeFile(__dirname + '/GeoData.json', response, function(err){
+                    //console.log(err);
+                })
+  }
+})
+
 
 // listen on port 3000
 app.listen(3000);
