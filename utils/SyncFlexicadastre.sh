edbba78f8b734810eef147f2eabb220e043487d8
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-11-17 14:17:40 +0000

    Check for folders being present in the server.

--- utils/SyncFlexicadastre.sh
+++ utils/SyncFlexicadastre.sh
@@ -6,9 +6,15 @@
 # TODO: How to check if there was a new file downloaded?
 # TODO: If new file downloaded, then run python scripts that checks for
 #       changes against CartoDB tables.
+# TODO: Keep output messages on a logfile in a log directory.
 
 remote_dir="http://data.pudo.org/flexicadastre"
 local_dir="/home/dev/data/flexicadastre"
 
-cd $local_dir
-wget -r -l1 --no-parent -A "flexiscrape*tgz" -nd -nc $remote_dir
+#Check if destination folder exits, exit otherwise
+if [ -d $local_dir ]; then
+    cd $local_dir   
+    wget -r -l1 --no-parent -A "flexiscrape*tgz" -nd -nc $remote_dir
+else
+    echo "Destination folder doesn't exist. The data haven't been synchronized."
+fi
