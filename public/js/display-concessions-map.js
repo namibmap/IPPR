1e78200be573d3835448ac0985ec2138120e72ce
missfunmi <missfunmi@me.com>
2015-12-13 19:15:45 +0000

    Highlight concessions in the map when a license is clicked. For #40

--- public/js/display-concessions-map.js
+++ public/js/display-concessions-map.js
@@ -17,9 +17,12 @@ $.get('/data', function(data) {
 });
 
 $('button.license-number').click(function(event) {
-  var clickedLicense = $( this ).attr('id');
-  toggleMapElementForClickedLicense(clickedLicense, licenseLabels);
-  toggleMapElementForClickedLicense(clickedLicense, licensePolygons);
+  // Highlight the clicked license in the list & un-highlight the others
+  $(this).addClass('active').siblings().removeClass('active');
+
+  // Highlight the concessions block in the map for the selected license
+  var clickedLicense = $(this).attr('id');
+  highlightGeojsonForClickedLicense(clickedLicense);
 });
 
 function createMapWithGeoJsonFeatures(geojsonFeatures) {
@@ -37,7 +40,8 @@ function createMapWithGeoJsonFeatures(geojsonFeatures) {
   // Add PELs to the map
   geojsonFeatures.forEach(function(feature) {
     var polygon = L.geoJson(feature, {
-      onEachFeature: onEachFeature
+      onEachFeature: onEachFeature,
+      style: defaultStyle
     }).addTo(map);
 
     // Store PEL polygons for filtering later
@@ -50,54 +54,71 @@ function createMapWithGeoJsonFeatures(geojsonFeatures) {
   });
 }
 
-function onEachFeature(feature, layer) {
-    if (feature.properties) {
-      var concession_number = feature.properties.concession_number;
-      var license_number = feature.properties.license_number;
-      var company_name = feature.properties.company_name;
-      var popupContent = 'Concession: ' + concession_number + 
-          '<br/>License: ' + license_number + '<br/>Company: ' + company_name;        
+function highlightGeojsonForClickedLicense(clickedLicense) {
+  // Highlight all polygons for the clicked license
+  var clickedLicensePolygons = licensePolygons[clickedLicense];
+  clickedLicensePolygons.forEach(function(element) {
+    element.setStyle(highlightedStyle());
+  });  
 
-      // Display a popup when the polygon is clicked
-      layer.bindPopup(popupContent);
-
-      // Label each polygon & display a popup when clicked as well
-      var label = L.marker(layer.getBounds().getCenter(), {
-        icon: L.divIcon({
-          className: 'concessions-text-label',
-          html: concession_number,
-          iconSize: [100, 40]
-        })
-      })
-      .bindPopup(popupContent)
-      .addTo(map);
-
-      // Store polygon labels for filtering later
-      if (licenseLabels.hasOwnProperty(license_number)) {
-        licenseLabels[license_number].push(label);
-      } else {
-        licenseLabels[license_number] = [label];
-      }
+  // Reset polygons style for the remaining licenses
+  for (var license in licensePolygons) {    
+    if (license !== clickedLicense) {
+      var remainingPolygons = licensePolygons[license];      
+      remainingPolygons.forEach(function(element) {
+        element.resetStyle(element);
+      });
     }
+  }
 }
 
-function toggleMapElementForClickedLicense(clickedLicense, licenseToMapElements) {
-  for (var license in licenseToMapElements) {
-    if (license !== clickedLicense) {
-      // Hide PEL map elements for all other licenses except the one clicked
-      var notClickedLicenseMapElements = licenseToMapElements[license];
-      notClickedLicenseMapElements.forEach(function(element) {
-        map.removeLayer(element);
-      });
+function defaultStyle(feature) {
+  return {
+    weight: 2,
+    opacity: 1,
+    color: '#7B00B4',       // line color
+    fillColor: '#C994C7',   // polygon background color
+    fillOpacity: 0.7
+  }
+}
+
+function highlightedStyle(feature) {
+  return {
+    weight: 2,
+    opacity: 1,
+    color: '#91003F',       // line color
+    fillColor: '#FEB24C',   // polygon background color
+    fillOpacity: 0.7
+  }
+}
+
+function onEachFeature(feature, layer) {
+  if (feature.properties) {
+    var concession_number = feature.properties.concession_number;
+    var license_number = feature.properties.license_number;
+    var company_name = feature.properties.company_name;
+    var popupContent = 'Concession: ' + concession_number + 
+        '<br/>License: ' + license_number + '<br/>Company: ' + company_name;        
+
+    // Display a popup when the polygon is clicked
+    layer.bindPopup(popupContent);
+
+    // Label each polygon & display a popup when clicked as well
+    var label = L.marker(layer.getBounds().getCenter(), {
+      icon: L.divIcon({
+        className: 'concessions-text-label',
+        html: concession_number,
+        iconSize: [100, 40]
+      })
+    })
+    .bindPopup(popupContent)
+    .addTo(map);
+
+    // Store polygon labels for later use
+    if (licenseLabels.hasOwnProperty(license_number)) {
+      licenseLabels[license_number].push(label);
     } else {
-      // Display the PEL map element for the clicked license
-      var clickedLicenseMapElements = licenseToMapElements[clickedLicense];
-      clickedLicenseMapElements.forEach(function(element) {
-        // If it was hidden on a previous click, re-add it to the map
-        if (!(map.hasLayer(element))) {
-          map.addLayer(element)
-        }
-      });
+      licenseLabels[license_number] = [label];
     }
   }
 }
